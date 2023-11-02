d981debad35f66552ed113e72c2fb08e504be813
Chase Brock <chase.p.brock@gmail.com>
2023-11-02 02:05:11 +0000

    react-router imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+}   from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
